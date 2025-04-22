274ab27dc4d0cb8435271b7a6ed0654108246a95
hidirektor <hidirektor@gmail.com>
2025-04-22 19:32:10 +0300

    Initial Commit

---
+++ Mavedda/MaveddaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaveddaApp.swift
+//  Mavedda
+//
+//  Created by Halil İbrahim Direktör on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MaveddaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaveddaTests/MaveddaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MaveddaTests.swift
+//  MaveddaTests
+//
+//  Created by Halil İbrahim Direktör on 22.04.2025.
+//
+
+import Testing
+@testable import Mavedda
+
+struct MaveddaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
